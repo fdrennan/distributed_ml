a4dbe3f344153e268f30829ef1cd1e529b7c6f1a
Freddy Drennan <drennanfreddy@gmail.com>
2018-10-09 17:46:07 -0600

    remove my_model

---
+++ distributed_ml.py
@@ -0,0 +1,124 @@
+import boto3
+import configparser
+from user_data import CreateUserData
+import os
+import datetime
+import shutil
+
+class Distributor():
+
+    NOW = datetime.datetime.now()
+
+    config_exists = os.path.isfile('configuration.ini')
+
+    if config_exists:
+
+        s3 = boto3.resource('s3')
+        ec2 = boto3.resource('ec2')
+
+        config = configparser.ConfigParser()
+        config.read('configuration.ini')
+
+        BUCKET_NAME = config['BUCKETS']['BUCKET_NAME']
+        LOCAL_DIR = config['DATA_DIR']['LOCAL']
+        SERVER_DIR = config['DATA_DIR']['LOCAL']
+        AMI = config['IMAGES']['UBUNTU_1808']
+        RUNNING_IDS = dict(config['EC2_IDS'])
+        # Connecting to S3
+        BUCKET = boto3.resource('s3').Bucket(BUCKET_NAME)
+        MODEL_DIRECTORY = config['MODEL_DATA']['model_directory']
+        SCRIPT = config['MODEL_DATA']['stript_to_run']
+
+
+        user_data = CreateUserData()
+
+    def __init__(self):
+        if self.config_exists:
+            print("Configuration File Found!")
+        else:
+            print("Please set up a configuration file. .")
+
+    def update_config(self):
+        config = configparser.ConfigParser()
+        self.config.read('configuration.ini')
+
+    def update_ids(self):
+        self.RUNNING_IDS = dict(self.config['EC2_IDS'])
+
+    def get_reservations(self):
+
+        RESERVATIONS = boto3.client('ec2').describe_instances().get('Reservations')
+
+        public_ips = []
+        for state in range(0, len(RESERVATIONS)):
+            state_logical = RESERVATIONS[state]['Instances'][0]['State']['Name']
+            running.append(state_logical)
+            if state_logical == 'running':
+                public_ips.append(RESERVATIONS[state]['Instances'][0]['NetworkInterfaces'][0]['PrivateIpAddresses'][0]['Association']['PublicIp'])
+
+        self.RUNNING = running
+        self.PUBLIC_IPS = public_ips
+        self.RESERVATIONS = RESERVATIONS
+
+        return(running, public_ips)
+
+    def download_file(self, origin, destination):
+        self.origin = origin
+        self.destination = destination
+        self.BUCKET.download_file(self.origin, self.destination)
+
+    def upload_file(self):
+        shutil.make_archive(self.MODEL_DIRECTORY, 'zip', self.MODEL_DIRECTORY)
+        self.origin = self.MODEL_DIRECTORY + '.zip'
+        self.destination = self.MODEL_DIRECTORY + '.zip'
+        self.BUCKET.upload_file(self.origin, self.destination)
+
+    def launch_instances(self, n, size):
+
+        res = self.ec2.create_instances(KeyName="Shiny",
+                                     InstanceType=size,
+                                     MinCount=1,
+                                     MaxCount=n,
+                                     SecurityGroupIds=[
+                                         'launch-wizard-8'
+                                     ],
+                                     UserData=self.user_data,
+                                     ImageId=self.AMI
+                                     )
+
+        self.update_config()
+        self.update_ids()
+
+        self.LEN_RUNNING = len(self.RUNNING_IDS)
+        self.list_ids = {'server_' + str(key + self.LEN_RUNNING): res[key].instance_id for key in range(len(res))}
+
+        # if len(self.RUNNING_IDS) != 0:
+        #     self.ids = self.ids + self.RUNNING_IDS
+        self.RUNNING_IDS.update(self.list_ids)
+
+        self.config['EC2_IDS'] = self.RUNNING_IDS
+        with open('configuration.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def clear_instances(self):
+        self.config['EC2_IDS'] = {}
+        with open('configuration.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def terminate_instances(self):
+        self.ids = list(self.RUNNING_IDS.values())
+        if len(self.ids) != 0:
+            self.terminated = self.ec2.instances.filter(InstanceIds=list(self.ids)).terminate()
+            self.clear_instances()
+            self.update_config()
+            self.update_ids()
+
+
+D = Distributor()
+D.upload_file()
+D.launch_instances(1, 't2.medium')
+# D.terminate_instances()
+print(D.user_data)
+# D.upload_file()
+
+# aws s3 cp my_model.zip s3://awscar/my_model.zip
--- user_data.py
+++ user_data.py
@@ -4,24 +4,51 @@ def CreateUserData():
     config = configparser.ConfigParser()
     config.read('configuration.ini')
 
-    USERNAME = BUCKET_NAME = config['CONNECTION']['USERNAME']
-    PASSWORD = BUCKET_NAME = config['CONNECTION']['PASSWORD']
+    USERNAME         = config['CONNECTION']['USERNAME']
+    PASSWORD         =  config['CONNECTION']['PASSWORD']
+    GIT_USERNAME     = config['GIT']['git_username']
+    GIT_EMAIL        = config['GIT']['git_email']
+    GIT_REPO         = config['GIT']['repository']
+    MODEL_DIRECTORY  = config['MODEL_DATA']['model_directory']
+    BUCKET           = config['BUCKETS']['bucket_name']
+
+    REPOSITORY       = 'git clone ' + GIT_REPO
+    MODEL_ZIP        = MODEL_DIRECTORY + '.zip'
+    BUCKET_DIR       = BUCKET + "/" + MODEL_ZIP
+    DOWNLOAD_COMMAND = 'aws s3 cp s3://' + BUCKET_DIR + " " + MODEL_DIRECTORY + "/" + MODEL_ZIP
+    UNZIP            = 'unzip ' + MODEL_ZIP
 
     user_data ='''#!/bin/bash
     
+    echo 'user_data running correctly' >> where_we_at
+        
     cd /root
+    
     apt install apt-transport-https software-properties-common -y
+
+    echo "installing updates" >> where_we_at
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
     add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
     apt update -y
+    echo "installing R" >> where_we_at
     apt install r-base -y
     apt install r-base-core -y
     apt-get install build-essential -y
     
+    echo "Configuring git" >> where_we_at
+    
+    apt-get install git-core''' + '''git config --global user.name "''' + GIT_USERNAME + '''"
+    git config --global user.email "''' + GIT_EMAIL + '''"
+    ''' + REPOSITORY + '''
+    cp -r distributed_ml/. .
+    echo "installing Python" >> where_we_at
     apt install python-pip -y
     apt install python3-pip -y
-    pip3 install --upgrade pip
+    
     pip3 install sklearn matplotlib boto3
+    
+    echo "installing AWS" >> where_we_at
+    
     pip install awscli --upgrade
     echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
     source ~/.bashrc
@@ -31,12 +58,15 @@ def CreateUserData():
     touch ~/.aws/credentials
     echo '[default]' >> ~/.aws/credentials
     '''  + '''echo 'aws_access_key_id=''' + USERNAME + '''' >> ~/.aws/credentials
-    echo 'aws_secret_access_key=''' + PASSWORD + '''' >> ~/.aws/credentials
-
+    echo 'aws_secret_access_key=''' + PASSWORD + '''' >> ~/.aws/credentials''' + '''
     touch ~/.aws/config
     echo '[default]' >> ~/.aws/config
     echo 'region=us-east-2' >> ~/.aws/config
-    echo 'output=json' >> ~/.aws/config'''
+    echo 'output=json' >> ~/.aws/config
+    ''' + DOWNLOAD_COMMAND + '''
+    cd ''' +  MODEL_DIRECTORY + '''
+    ''' + UNZIP + '''
+    echo "Done" >> /root/where_we_at'''
 
     return user_data
 
